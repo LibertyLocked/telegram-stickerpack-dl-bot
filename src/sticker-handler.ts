820b6e1226ea94cde82077820ed22585f3c4ca76
libertylocked <unlock.liberty@gmail.com>
2017-08-03 22:37:15 -0400

    Add error messages

--- src/sticker-handler.ts
+++ src/sticker-handler.ts
@@ -8,32 +8,30 @@ export default (bot: TelegramBot) => async (msg: TelegramBot.API.IMessage) => {
   }
   const userSentSticker: TelegramBot.API.ISticker = msg.sticker;
 
-  // spin off some async tasks
-  // get the link to download the sticker file
-  const taskGetLink = bot.getFileLink(userSentSticker.file_id);
   // get the sticker set
   const taskGetStickerSet: Promise<TelegramBot.API.IStickerSet> = bot._request("getStickerSet",
     { form: { name: userSentSticker.set_name } });
-
-  // send user the download link
-  await bot.sendMessage(msg.chat.id, `The sticker can be downloaded at:\n ${await taskGetLink}\n`);
 
   // show user some info about the sticker pack
   const stickerSet = await taskGetStickerSet;
   await bot.sendMessage(msg.chat.id,
     `The sticker set is ${stickerSet.title} ` +
-    `([${stickerSet.name}](https://telegram.me/addstickers/${stickerSet.name})).\n` +
+    `(<a href="https://telegram.me/addstickers/${stickerSet.name}">${stickerSet.name}</a>)\n` +
     `Contains ${stickerSet.stickers.length} stickers\n` +
     `<strong>Please wait while I put all the stickers into a zip for you...</strong>`,
     { parse_mode: "HTML" });
 
-  // give user the download link to the zipped sticker pack
-  const zipBuffer = await getZippedStickersAsync(bot)(stickerSet.stickers);
-  bot.sendDocument(msg.chat.id, zipBuffer, {
-    reply_to_message_id: msg.message_id,
-  }, {
-    filename: `${stickerSet.name}.zip`,
-  });
+  try {
+    // give user the download link to the zipped sticker pack
+    const zipBuffer = await getZippedStickersAsync(bot)(stickerSet.stickers);
+    bot.sendDocument(msg.chat.id, zipBuffer, {
+      reply_to_message_id: msg.message_id,
+    }, {
+        filename: `${stickerSet.name}.zip`,
+      });
+  } catch (err) {
+    bot.sendMessage(msg.chat.id, "An error occurred while zipping the stickers. Please try again");
+  }
 
   console.log("zipped and sent", stickerSet.name);
 };
